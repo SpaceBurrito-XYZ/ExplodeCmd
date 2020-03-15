a7fda77f3447eef56522bc40c4ce9b355cbad7ad
Megan Harris <megsh294@gmail.com>
2020-03-14 21:10:27 -0400

    :rewind: remove handlers and proxy
    :rewind: remove the explosion handlers and the client/server proxy; makes more sense to have blocks drop items than to write a bunch of spaghetti to disable block drops
    
    Signed-off-by: Megan Harris <megsh294@gmail.com>

--- src/main/java/xyz/spaceburrito/mc/explodecmd/ExplodeCmd.java
+++ src/main/java/xyz/spaceburrito/mc/explodecmd/ExplodeCmd.java
@@ -4,13 +4,12 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
 import org.jetbrains.annotations.NotNull;
 import org.apache.logging.log4j.Logger;
 import xyz.spaceburrito.mc.explodecmd.common.Reference;
 import xyz.spaceburrito.mc.explodecmd.common.command.ExplodeCommand;
-import xyz.spaceburrito.mc.explodecmd.common.proxy.CommonProxy;
+
 
 
 @Mod(
@@ -24,9 +23,6 @@ public class ExplodeCmd {
     @Mod.Instance(Reference.MOD_ID)
     public static @NotNull ExplodeCmd INSTANCE;
 
-    @SidedProxy(clientSide = Reference.CLIENT_SIDE_PROXY_CLASS,serverSide = Reference.SERVER_SIDE_PROXY_CLASS)
-    public static CommonProxy proxy;
-
     public static Logger logger;
 
     @Mod.EventHandler
@@ -36,7 +32,7 @@ public class ExplodeCmd {
 
     @Mod.EventHandler
     public void init(FMLInitializationEvent event) {
-     proxy.init(event);
+     //proxy.init(event);
     }
 
     @Mod.EventHandler
